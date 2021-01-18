d4fe2fdd0d1e6d488e08ce9cc368c0a0c9286177
nbavaro1 <39228701+nbavaro1@users.noreply.github.com>
2021-01-17 21:59:11 -0500

    Update questionsPMP.js

--- questionsPMP.js
+++ questionsPMP.js
@@ -3,10 +3,11 @@ var questions = [
         "question": "The following are characteristics of a project except:",
         "option1" : "It is a temporary in nature",
         "option2": "It is continuous",
-        "option3": "It is unique",
+        "option3": "It is uniqueStakeholder expectations have been metIt is unique",
         "option4": "It has a definitive end",
         "answer": 2
     },
+    
     {
         "question": "When is a project considered successful?",
         "option1" : "All deliverables have been completed",
