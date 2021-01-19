39b2dbc89432b2cf83a15649e1a64aa5777a0bcd
nbavaro1 <39228701+nbavaro1@users.noreply.github.com>
2021-01-19 14:03:27 -0500

    Update questionsPMP.js

--- questionsPMP.js
+++ questionsPMP.js
@@ -136,5 +136,165 @@ var questions = [
         "option3": "A project selection method",
         "option4": "A feasibility study",
         "answer": 4
+    },
+     {
+        "question": "Which of the following is NOT an advantage of limiting work in progress?",
+        "option1" : "It reduces the potential need to rework a large collection of flawed, partially completed items.",
+        "option2": "It helps optimize throughput to make processes work more efficiently.",
+        "option3": "It brings bottlenecks in the production process to the surface so they can be identified and resolved.",
+        "option4": "It maximizes resource utilization make process work more efficiently.",
+        "answer": 4
+    },
+    {
+        "question": "The relationship between leadership and management in agile methods is:",
+        "option1" : "Leadership replaces all aspects of management.",
+        "option2": "Leadership is subsidiary to management.",
+        "option3": "Management and leadership are used together.",
+        "option4": "Management and leadership are incompatible.",
+        "answer": 3
+    },
+    {
+        "question": "The core values of XP include:",
+        "option1" : "Courage, communication, input",
+        "option2": "Simplicity, control, respect",
+        "option3": "Feedback, simplicity, communication",
+        "option4": "Solutions, feedback, control",
+        "answer": 3
+    },
+    {
+        "question": "In the Agile Manifesto, what is valued more than processes and tools?",
+        "option1" : "Customer collaboration",
+        "option2": "Individuals and interactions",
+        "option3": "Working software",
+        "option4": "Responding to change",
+        "answer": 2
+    },
+    {
+        "question": "The acronym WIP stands for:",
+        "option1": "Worth in performance",
+        "option2": "Work in progress",
+        "option3": "Waste in process",
+        "option4": "Work is  progressing",
+        "answer": 2
+    },
+    {
+        "question": "The three pillars of Scrum are:",
+        "option1": "Transparency, Communication, Adaptation",
+        "option2": "Inspection, Adaptation, Evolution",
+        "option3": "$Adaptation, Inspection, Communication",
+        "option4": "Transparency, Inspection, Adaptation",
+        "answer": 4
+    },
+    {
+        "question": "On a typical agile team, who has the best insight into task execution?",
+        "option1": "Project manager",
+        "option2": "Team members",
+        "option3": "Scrum Master",
+        "option4": "Agile coach",
+        "answer": 2
+    },
+    {
+        "question": "Which of the following Agile Manifesto values deals most closely with WIP?",
+        "option1": "Customer collaboration over contract negotiation",
+        "option2": "Css fileIndividuals and interactions over processes and tools",
+        "option3": "Working software over comprehensive documentation",
+        "option4": "Responding to change over following a plan",
+        "answer": 3
+    },
+    {
+        "question": "What is one of the three questions that are addressed in the daily scrum?",
+        "option1": "What have I done since the last daily scrum?",
+        "option2": "Why didn’t I get my work done yesterday?",
+        "option3": "How will I do my work for today?",
+        "option4": "What is my plan for the week?",
+        "answer": 1
+    },
+    {
+        "question": "What are three of the seven lean core concepts? ",
+        "option1": "Eliminate waste, empower the team, deter decisions",
+        "option2": "Build quality in, exterminate waste, optimize learning",
+        "option3": "Optimize the whole, defer decisions, amplify learning",
+        "option4": "Deliver decisions, amplify the team, evaluate waste",
+        "answer": 3
+    },
+    {
+      "question": "The four primary roles of a servant leader include: ",
+      "option1": "Shielding team members from interruptions",
+      "option2": "Resolving conflicts",
+      "option3": "Determining which stories to include in an iteration",
+      "option4": "Assigning tasks to the team members",
+      "answer": 1
+    },
+    {
+        "question": "In Scrum, the definition of done is created with the input of everyone except the: ",
+        "option1": "Development team",
+        "option2": "Product owner",
+        "option3": "ScrumMaster",
+        "option4": "Process owner",
+        "answer": 4
+    },
+    {
+        "question": "In the lean approach, which of the following wouldn’t be considered an example of one of the seven forms of waste?",
+        "option1": "The handoff between coding and testing",
+        "option2": "Testing the code",
+        "option3": "Code that is waiting for testing",
+        "option4": "Assigned a developer to work on two projects at the same time",
+        "answer": 2
+    },
+    {
+        "question": "The XP core practices include:",
+        "option1": "Whole team, planning games, small releases",
+        "option2": "One team, process game, big releases",
+        "option3": "Paired team, planning games, quick releases",
+        "option4": "Build team, process game, incremental releases",
+        "answer": 1
+    },
+    {
+        "question": "Which of the following is one of the planned opportunitesfor inspection and adaptation in the Scrum method?",
+        "option1": "Velocity review meeting",
+        "option2": "Sprint risk meeting",
+        "option3": "Daily scrum",
+        "option4": "Retrospective planning meeting",
+        "answer": 3
+    },
+    {
+        "question": "Which of the following is an Agile Manifesto value?",
+        "option1": "Individuals and interactions over following a plan",
+        "option2": "Working software over processes and tools",
+        "option3": "Responding to change over comprehensive documentation",
+        "option4": "Customer collaboration over contract negotiation",
+        "answer": 4
+    },
+    {
+        "question": "The agile triangle of constraints is said to be inverted from the traditional triangle because it allows:",
+        "option1": "Scope and time to vary instead of cost",
+        "option2": "Cost and time to vary instead of scope",
+        "option3": "Scope and cost to be fixed instead of time",
+        "option4": "Scope to vary while time and cost are fixed",
+        "answer": 4
+    },
+    {
+        "question": "Which of the following Agile Manifesto principles reflects the agile focus on team empowerment?",
+        "option1": "Working software is the primary measure of progress.",
+        "option2": "Welcome changing requirements, even late in development.",
+        "option3": "Simplicity –the art of maximizing the amount of work not done-is essential.",
+        "option4": "Build projects around motivated individuals.",
+        "answer": 4
+    },
+    {
+        "question": "The Kanban pull system means that:",
+        "option1": "Kanban team members “pull” work from each other, pairing up as needed.",
+        "option2": "Each time a work item is completed, the newxtwork item is “pulled” into that stage of the process.",
+        "option3": "Iterations are “pulled” into the process as needed to keep the work organized.",
+        "option4": "Kanban teams have shorter work queues, which means that the work takes longer to complete (as shown by Little’s Law).",
+        "answer": 2
+    },
+    {
+        "question": "Which of the following isn’t a core aspect of the agile mindset?",
+        "option1": "Welcome change",
+        "option2": "Learn through discovery",
+        "option3": "Respect the process",
+        "option4": "Deliver value continuously",
+        "answer": 3
     }
 ];
